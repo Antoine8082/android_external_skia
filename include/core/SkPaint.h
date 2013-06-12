38e3abc687610e08e525b5b9d59c73418c7db19d
Xin Qi <xqi@codeaurora.org>
2013-06-12 16:28:04 -0700

    Several skia tunings
    * Optiming constructor of Paint object
    * Special copy path for small object
    * Global Language list instead of constructing a new obj everytime.
    
    Change-Id: Ic2ed4488a94e57d7192028ec4c608989837d756e

--- include/core/SkPaint.h
+++ include/core/SkPaint.h
@@ -15,6 +15,7 @@
 #include "SkDrawLooper.h"
 #include "SkXfermode.h"
 #ifdef SK_BUILD_FOR_ANDROID
+#include <pthread.h>
 #include "SkLanguage.h"
 #endif
 
@@ -42,6 +43,26 @@ typedef const SkGlyph& (*SkDrawCacheProc)(SkGlyphCache*, const char**,
                                            SkFixed x, SkFixed y);
 
 typedef const SkGlyph& (*SkMeasureCacheProc)(SkGlyphCache*, const char**);
+
+class SkLangList {
+    public:
+        SkLangList();
+
+    SkLanguage s;
+    SkLangList *next;
+};
+
+class SkLanguages {
+public:
+    SkLanguages();
+    SkLangList * setLanguage( const SkLanguage& locale );
+    const SkLanguage& getLanguage( SkLangList * t ) const;
+
+private:
+    SkLangList * LocaleArray;
+    pthread_mutex_t update_mutex;
+};
+
 
 /** \class SkPaint
 
@@ -658,7 +679,8 @@ public:
     /** Return the paint's language value used for drawing text.
         @return the paint's language value used for drawing text.
     */
-    const SkLanguage& getLanguage() const { return fLanguage; }
+    const SkLanguage& getLanguage() const;
+
 
     /** Set the paint's language value used for drawing text.
         @param language set the paint's language value for drawing text.
@@ -974,6 +996,8 @@ public:
                                       Style) const;
 
 private:
+    //Be noted to update SkPaint::SkPaint(const SkPaint& src) copy
+    //constructor when struture is changed for fast path!
     SkTypeface*     fTypeface;
     SkScalar        fTextSize;
     SkScalar        fTextScaleX;
@@ -1009,7 +1033,7 @@ private:
         kNoDrawAnnotation_PrivFlag  = 1 << 0,
     };
 #ifdef SK_BUILD_FOR_ANDROID
-    SkLanguage      fLanguage;
+    SkLangList*     fpLanguage;
     FontVariant     fFontVariant;
 #endif
 
