561a96f331962a618d1964de7652502237db02ed
PrimeDirective <activethrasher00@gmail.com>
2013-08-21 14:58:06 -0400

    remove Wno-error=strict-aliasing from skia
    this builds fine.  haven't had reports of any skia related problems yet on FLO
    
    Change-Id: I70f13f974736afbb138e058358c1e6879c350a4c
    
    Conflicts:
    	Android.mk

--- Android.mk
+++ Android.mk
@@ -494,15 +494,9 @@ LOCAL_C_INCLUDES := \
 	external/expat/lib
 
 
-# FIXME this should really be limited to files that need it, such as
-# src/utils/SkCamera.cpp -- pretty bad violations going on in there,
-# but most of the rest of skia is good
-LOCAL_CFLAGS += -fno-strict-aliasing
-
-# FIXME this should really be limited to files that need it, such as
-# src/utils/SkCamera.cpp -- pretty bad violations going on in there,
-# but most of the rest of skia is good
-LOCAL_CFLAGS += -fno-strict-aliasing
+# if all else fails...
+#LOCAL_CFLAGS += -fno-strict-aliasing
+#LOCAL_CFLAGS += -Wno-error=strict-aliasing
 
 LOCAL_LDLIBS += -lpthread
 
